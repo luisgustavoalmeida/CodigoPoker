fe2ada4209fda4abb38c0666edee35f9a8fb721e
Luís Gustavo de Almeida <luis.gustavo@engenharia.ufjf.br>
2023-08-21 11:04:16 -0300

    Codigo poker mestre

--- Origem_pg.py
+++ Origem_pg.py
@@ -137,13 +137,13 @@ def carregado_origem(id, senha, url, navegador):#navegador
             if (pyautogui.pixelMatchesColor(215, 1000, (36, 37, 38), tolerance=5)  # mensagem do canto inferior esquedo " Você esta offiline no momento."
                   or pyautogui.pixelMatchesColor(715, 650, (32, 33, 36), tolerance=5)  # fundo cinza com o dinoçauro
                   or pyautogui.pixelMatchesColor(700, 650, (255, 255, 255), tolerance=2)  # retangulo branco no meio da tela quando esta sem internete
-                  or pyautogui.pixelMatchesColor(700, 650, (221, 221, 221), tolerance=2)):  # tela cinza clara com cara ctiste
+                  or pyautogui.pixelMatchesColor(700, 650, (221, 221, 221), tolerance=7)):  # tela cinza clara com cara ctiste
                 print("aguarda 5 segundos e faz um novo teste se a pagina nao carregou")
                 time.sleep(5)
                 if (pyautogui.pixelMatchesColor(215, 1000, (36, 37, 38), tolerance=5)  # mensagem do canto inferior esquedo " Você esta offiline no momento."
                         or pyautogui.pixelMatchesColor(715, 650, (32, 33, 36), tolerance=5)  # fundo cinza com o dinoçauro
                         or pyautogui.pixelMatchesColor(700, 650, (255, 255, 255), tolerance=2)  # retangulo branco no meio da tela quando esta sem internete
-                        or pyautogui.pixelMatchesColor(700, 650, (221, 221, 221), tolerance=2)):  # tela cinza clara com cara ctiste
+                        or pyautogui.pixelMatchesColor(700, 650, (221, 221, 221), tolerance=7)):  # tela cinza clara com cara ctiste
                     print("Falha na pagina e a tela esta branca, da um F5")
                     IP.tem_internet()
                     pyautogui.press('f5')
