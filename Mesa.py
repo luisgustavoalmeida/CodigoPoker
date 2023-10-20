ece2074f1e5deefb8573210006a3f6c21ae7ee30
Luís Gustavo de Almeida <luis.gustavo@engenharia.ufjf.br>
2023-10-19 21:13:26 -0300

    Codigo poker mestre

--- Mesa.py
+++ Mesa.py
@@ -686,8 +686,8 @@ def joga_uma_vez(x_origem, y_origem):
 
 
 def dia_de_jogar_mesa(x_origem, y_origem, dia_da_semana, valor_fichas, time_rodou, roleta):
-    if dia_da_semana == 6 or dia_da_semana == 5 or dia_da_semana == 4 or dia_da_semana == 3 or dia_da_semana == 2 or dia_da_semana == 1 or dia_da_semana == 0:  # testa se é sabado ou domingo
-            # 0 domingo 1 segunda 2 terça 3 quarta 4 quinta 5 sexta 6 sabado
+    if dia_da_semana == 6 or dia_da_semana == 5 or dia_da_semana == 0:  # testa se é sabado ou domingo
+            # 0 domingo, 1 segunda, 2 terça, 3 quarta, 4 quinta, 5 sexta, 6 sabado
             #if pyautogui.pixelMatchesColor((x_origem + 750), (y_origem + 38), (245, 218, 96), tolerance=10) or pyautogui.pixelMatchesColor((x_origem + 802), (y_origem + 38), (245, 218, 96), tolerance=10) or (100000 < valor_fichas < 400000):
         print('conta para jogar mesa')
         if roleta == 'roleta_2':
