60a49c6362563d71baf35723d075b41a0712bb3f
Luís Gustavo de Almeida <luis.gustavo@engenharia.ufjf.br>
2023-11-17 16:00:51 -0300

    Codigo poker mestre

--- Tarefas.py
+++ Tarefas.py
@@ -350,7 +350,8 @@ def testa_continuar_fazendo_tarefa(x_origem, y_origem, id, senha, url, navegador
 def recolher_tarefa_upando(x_origem, y_origem):
     status_tarefas = "Não tem missão"
     # print("recolher_tarefa_upando")
-    if pyautogui.pixelMatchesColor((x_origem + 627), (y_origem + 35), (228, 194, 31), tolerance=5):  # testa se tem que recolher icone das tarefas amarelo
+    if (pyautogui.pixelMatchesColor((x_origem + 627), (y_origem + 35), (228, 194, 31), tolerance=5)
+            and not pyautogui.pixelMatchesColor((x_origem + 627), (y_origem + 35), (119, 168, 219), tolerance=5)):  # testa se tem que recolher icone das tarefas amarelo
         print('Tem missão para recolher, aguarda um tempo pequeno')
         status_tarefas = "Recolhido"
         #time.sleep(2)
