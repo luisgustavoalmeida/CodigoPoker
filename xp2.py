1fd756fcc4efd8f2eeae79a50627fe8ed42436ad
Luís Gustavo de Almeida <luis.gustavo@engenharia.ufjf.br>
2023-12-03 06:58:56 -0300

    Codigo poker mestre

--- xp2.py
+++ xp2.py
@@ -11,13 +11,14 @@ def pega_2xp(x_origem, y_origem):
         pyautogui.doubleClick(x_origem + 260, y_origem + 25)  # clica no no icone dos meus objetos
         Limpa.limpa_pequeno(x_origem, y_origem)
         if pyautogui.pixelMatchesColor((x_origem + 241), (y_origem + 146), (250, 112, 1), tolerance=10):
-            # testa se esta meus objetos
+            # testa se esta meus objetos esta visivel
             print("Janela dos meus objetos visivel")
             pyautogui.doubleClick(x_origem + 260, y_origem + 25)  # clica no no icone dos meus objetos
             time.sleep(1)
             pyautogui.doubleClick(x_origem + 260, y_origem + 25)  # clica no no icone dos meus objetos
-            if pyautogui.pixelMatchesColor((x_origem + 230), (y_origem + 222), (152, 48, 220), tolerance=10):
-                # testa se tem a guia de 2xp
+            if (pyautogui.pixelMatchesColor((x_origem + 230), (y_origem + 222), (152, 48, 220), tolerance=10)
+                    and pyautogui.pixelMatchesColor((x_origem + 241), (y_origem + 146), (250, 112, 1), tolerance=10)):
+                # testa se tem a guia de 2xp # testa se esta meus objetos esta visivel
                 print("Tem a guia de 2xp")
                 pyautogui.doubleClick(x_origem + 260, y_origem + 25)  # clica no no icone dos meus objetos
                 pyautogui.doubleClick(x_origem + 230, y_origem + 222)  # clica na guia do 2xp
@@ -34,7 +35,9 @@ def pega_2xp(x_origem, y_origem):
                     print('2 XP Acionado')
                     time.sleep(1)
                     # return "2XP acionado"
-            else:
+            elif (not pyautogui.pixelMatchesColor((x_origem + 230), (y_origem + 222), (152, 48, 220), tolerance=10)
+                  and pyautogui.pixelMatchesColor((x_origem + 241), (y_origem + 146), (250, 112, 1), tolerance=10)):
+                # testa se não tem a guia de 2xp # testa se esta meus objetos esta visivel
                 print('Não tem a guia de 2xp')
                 pyautogui.doubleClick(x_origem + 260, y_origem + 25)  # clica no no icone dos meus objetos
                 pyautogui.click(x_origem + 772, y_origem + 160)  # clica no fechar meus objetos
