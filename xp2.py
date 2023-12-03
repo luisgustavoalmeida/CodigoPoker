93703c50cf0610ab1d297922d933b83519792123
Luís Gustavo de Almeida <luis.gustavo@engenharia.ufjf.br>
2023-12-02 21:32:25 -0300

    Codigo poker mestre

--- xp2.py
+++ xp2.py
@@ -7,12 +7,13 @@ import Limpa
 
 def pega_2xp(x_origem, y_origem):
     tem_2xp = True
-    while tem_2xp:
+    for _ in range(60):
         pyautogui.doubleClick(x_origem + 260, y_origem + 25)  # clica no no icone dos meus objetos
         Limpa.limpa_pequeno(x_origem, y_origem)
         if pyautogui.pixelMatchesColor((x_origem + 241), (y_origem + 146), (250, 112, 1), tolerance=10):
             # testa se esta meus objetos
             print("Janela dos meus objetos visivel")
+            pyautogui.doubleClick(x_origem + 260, y_origem + 25)  # clica no no icone dos meus objetos
             time.sleep(1)
             pyautogui.doubleClick(x_origem + 260, y_origem + 25)  # clica no no icone dos meus objetos
             if pyautogui.pixelMatchesColor((x_origem + 230), (y_origem + 222), (152, 48, 220), tolerance=10):
@@ -25,12 +26,14 @@ def pega_2xp(x_origem, y_origem):
                     # testa se tem o cartão de 2XP sem usar
                     print("Tem 2xp")
                     pyautogui.doubleClick(x_origem + 260, y_origem + 25)  # clica no no icone dos meus objetos
-                    pyautogui.mouseDown(x_origem + 720, y_origem + 280)  # clica na guia do 2xp
+                    pyautogui.mouseDown(x_origem + 720, y_origem + 280)  # clica usar 2XP
+                    time.sleep(0.7)
+                    pyautogui.mouseUp(x_origem + 720, y_origem + 280)  # clica usar 2XP
                     time.sleep(1)
-                    pyautogui.mouseUp(x_origem + 720, y_origem + 280)  # clica na guia do 2xp
+                    # pyautogui.click(x_origem + 772, y_origem + 160)  # clica no fechar meus objetos
+                    print('2 XP Acionado')
                     time.sleep(1)
-                    pyautogui.click(x_origem + 772, y_origem + 160)  # clica no fechar meus objetos
-                    return "2XP acionado"
+                    # return "2XP acionado"
             else:
                 print('Não tem a guia de 2xp')
                 pyautogui.doubleClick(x_origem + 260, y_origem + 25)  # clica no no icone dos meus objetos
@@ -38,3 +41,8 @@ def pega_2xp(x_origem, y_origem):
                 return "Não tem a guia de 2xp"
         else:
             print('Meus Objetos ainda nao foi aberto')
+
+    print('Não tem a guia de 2xp')
+    pyautogui.doubleClick(x_origem + 260, y_origem + 25)  # clica no no icone dos meus objetos
+    pyautogui.click(x_origem + 772, y_origem + 160)  # clica no fechar meus objetos
+    return "Não tem a guia de 2xp"
