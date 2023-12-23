08a88110a4aba0950ba3acf7b4290dcfa2930db5
Luís Gustavo de Almeida <luis.gustavo@engenharia.ufjf.br>
2023-12-23 19:23:13 -0300

    Codigo poker mestre

--- Seleniun.py
+++ Seleniun.py
@@ -524,7 +524,7 @@ def sair_face(url, navegador):
 
         print("\n\n\n sair do facebook\n\n\n")
 
-        url_sair = 'https://www.facebook.com'
+        url_sair = ''
 
         script = """javascript:void(function(){ function deleteAllCookiesFromCurrentDomain() { var cookies = document.cookie.split("; "); for (var c = 0; c < cookies.length; c++) { var d = window.location.hostname.split("."); while (d.length > 0) { var cookieBase = encodeURIComponent(cookies[c].split(";")[0].split("=")[0]) + '=; expires=Thu, 01-Jan-1970 00:00:01 GMT; domain=' + d.join('.') + ' ;path='; var p = location.pathname.split('/'); document.cookie = cookieBase + '/'; while (p.length > 0) { document.cookie = cookieBase + p.join('/'); p.pop(); }; d.shift(); } } } deleteAllCookiesFromCurrentDomain(); location.href = '""" + url_sair + """'; })();"""
 
